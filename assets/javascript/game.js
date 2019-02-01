9766b5e660fe88aaf06918c3c9369ad3fa0af173
Bowie Sessions <bowie@Bowies-MacBook-Pro.local>
2019-01-31 23:31:10 -0600

    Minor corrections and proper commenting and styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// All defined variables for the page.
 let
     win = 0,
     loss = 0,
@@ -9,25 +10,31 @@ let
     emerald = 0,
     sapphire = 0;
 
+// Random Number Generator for all functional needs throughout the page
 let rng = function (x, y) {
     return Math.floor((Math.random() * x) + y);
 }
 
+// Core game chassis, which is used when page loads, and on every reset of the game.
 let gameCore = function () {
-    crystalNumber = rng(102,19);
+    // Defines the crystal number (the target to be achieved)
+    crystalNumber = rng(102, 19);
+    // Places the crystal number, wins, losses, and sets current total to 0.
     $(".crystalNumber").html(crystalNumber);
     $(".winHolder").html("Wins: " + win)
     $(".lossHolder").html("Losses: " + loss)
     currentScore = 0;
     $(".score").html(currentScore);
+    // Defines the value of each crystal button.
     diamond = rng(12, 1);
     amber = rng(12, 1);
     emerald = rng(12, 1);
     sapphire = rng(12, 1);
 }
-
+// Starting the game off right.
 gameCore();
 
+// The four click functions for each of the crystal buttons.
 $(".diamond").on("click", function () {
     currentScore = currentScore + diamond;
     $(".score").html(currentScore);
@@ -49,7 +56,8 @@ $(".sapphire").on("click", function () {
     winLoss();
 });
 
-let winLoss = function() {
+// The function which decides win and loss.
+let winLoss = function () {
     if (parseInt(currentScore) === parseInt(crystalNumber)) {
         win++;
         gameCore();
@@ -59,6 +67,3 @@ let winLoss = function() {
         gameCore();
     }
 }
-// Make .score field check against .randomNumber
-
-            
